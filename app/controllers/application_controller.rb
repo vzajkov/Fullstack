a7e5c019f61e2c2159db43e21321e209b56b8350
Valery Zajkov <wuvalzaj@gmail.com>
2017-10-24 12:31:21 -0400

    Complete application and user controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def login(user)
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logout
+    session[:session_token] = nil
+    @current_user.reset_session_token!
+  end
+
+  def logged_in?
+    !!@current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: params[:session_token])
+  end
+
+  def ensure_logged_in
+    redirect_to new_session_url unless logged_in?
+  end
+
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login(@user)
+      redirect_to reviews_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
