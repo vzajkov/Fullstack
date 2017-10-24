8486cad6bcf47ee8f5b037988c34bbf7d6c20090
Valery Zajkov <wuvalzaj@gmail.com>
2017-10-24 14:11:12 -0400

    Complete sessionsController, edit GemFile, set up package.json and webpack.config.js files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user
+      login(@user)
+      redirect_to reviews_url
+    else
+      flash.now[:errors] = ["Incorrect credentials"]
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+
 end
