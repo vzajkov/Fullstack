85cb919dcbbba6384ecd4e08010d507415bd5228
Valery Zajkov <wuvalzaj@gmail.com>
2017-10-25 17:31:15 -0400

    Add landing page

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import SessionFormContainer from './session/session_form_container.jsx';
-import { Route, Link } from 'react-router-dom';
-
+import { Route, Link, Switch } from 'react-router-dom';
+import Landing from './landing.jsx';
 const App = () => (
-  <div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/login" component={SessionFormContainer} />
+        <Route path="/signup" component={SessionFormContainer} />
+      </Switch>
+  </div>);
 
-    <div className="header">
-        <span class="Kelp">
 
-          <span id="Kelp-title">Kelp  </span>
-          <i className="fa fa-yelp" aria-hidden="true"></i>
-        </span>
-        <ul className='headerLinks'>
-          <li><Link to="/login">Login </Link> </li>
-          <li><Link to="/signup">Signup </Link> </li>
-        </ul>
-    </div>
 
-    <div className="footer">
-      <div className="footerLinks">
-        <Link to="#">Github</Link>
-        <Link to="#">MySpace</Link>
-        <Link to="#">LinkedIn</Link>
-      </div>
-    </div>
-
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
-
-  </div>
-);
 
 
 export default App;
---
+++ frontend/components/landing.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import SessionFormContainer from './session/session_form_container.jsx';
+import { Route, Link, Switch } from 'react-router-dom';
+
+const Landing = () => (
+  <div>
+
+    <div className="header">
+        <span className="Kelp">
+
+          <span id="Kelp-title">Kelp  </span>
+          <i className="fa fa-yelp" aria-hidden="true"></i>
+       </span>
+        <ul className='headerLinks'>
+          <li><Link to="/login">Login</Link> </li>
+          <li><Link to="/signup">Signup</Link> </li>
+        </ul>
+    </div>
+
+
+    <img id="landing_img" src="https://goo.gl/JzKBj1"></img>
+
+
+    <div className="footer">
+      <div className="footerLinks">
+        <Link to="#">Github</Link>
+        <Link to="#">MySpace</Link>
+        <Link to="#">LinkedIn</Link>
+      </div>
+    </div>
+
+    <Route path="/login" component={SessionFormContainer} />
+    <Route path="/signup" component={SessionFormContainer} />
+  </div>
+);
+
+export default Landing;
