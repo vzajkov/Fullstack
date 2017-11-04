88d48d41321ca6899c47e60acf4af5551b9e8dfd
Valery Zajkov <wuvalzaj@gmail.com>
2017-11-04 19:54:31 -0400

    Fix error in constructor

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -9,7 +9,7 @@ class SessionForm extends React.Component {
       username: '',
       password: ''
     };
-    this.handleClick = this.handleDemoLogin.bind(this);
+    this.handleDemoLogin = this.handleDemoLogin.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
