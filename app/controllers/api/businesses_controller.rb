42609fff3a78d5ab53e5677b0f428a9f6f9794f4
Valery Zajkov <wuvalzaj@gmail.com>
2017-11-29 18:32:33 -0500

    Edit review form styling and remove N+1 issue in filtering

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -2,37 +2,37 @@
 
   def index
 
-    @businesses = Business.all
+    # @businesses = Business.all
 
-    @businesses = Business.joins(:detailed_info)
+    @businesses = Business.includes(:detailed_info, :address, :reviews)
     #Template referenced from "Implementing Full Text Search in Rails with Postgres", by Ryan Stenberg
     if params[:name] && params[:name] != ""
       @businesses = @businesses.search_for(params[:name])
     end
     #End of template reference
     if params[:takeout] == "true"
-      @businesses = @businesses.where(:detailed_infos => {:takeout => params[:takeout]})
+      @businesses = @businesses.where(:detailed_infos => {:takeout => params[:takeout]}).includes(:detailed_info, :address, :reviews)
     end
 
     if params[:wifi] == "true"
-      @businesses = @businesses.where(:detailed_infos => {:wifi => params[:wifi]})
+      @businesses = @businesses.where(:detailed_infos => {:wifi => params[:wifi]}).includes(:detailed_info, :address, :reviews)
     end
 
     if params[:parking] == "true"
-      @businesses = @businesses.where(:detailed_infos => {:parking => params[:parking]})
+      @businesses = @businesses.where(:detailed_infos => {:parking => params[:parking]}).includes(:detailed_info, :address, :reviews)
     end
 
     if params[:smoking] == "true"
-      @businesses = @businesses.where(:detailed_infos => {:smoking => params[:smoking]})
+      @businesses = @businesses.where(:detailed_infos => {:smoking => params[:smoking]}).includes(:detailed_info, :address, :reviews)
     end
 
     if params.values_at(:smoking, :wifi, :parking, :takeout).all? {|v| v.nil?}
-      @businesses = Business.all
+      @businesses = Business.all.includes(:detailed_info, :address, :reviews)
     end
 
     price_params = params.keys.select {|k| params[k] == "true" && ["d","dd","ddd","dddd"].include?(k)}
     if !price_params.empty?
-      @businesses = @businesses.where(:detailed_infos => {:price => price_params})
+      @businesses = @businesses.where(:detailed_infos => {:price => price_params}).includes(:detailed_info, :address, :reviews)
     end
 
     render 'businesses/index'
--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -7,10 +7,20 @@ class ReviewForm extends React.Component {
       rating: "1",
       body: "fake review",
       business_id: this.props.currentBusiness.id,
-      author_id: this.props.currentUser.id
+      author_id: this.props.currentUser.id,
+      oneStar: false,
+      twoStar: false,
+      threeStar: false,
+      fourStar: false,
+      fiveStar: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
+    this.toggleOne = this.toggleOne.bind(this);
+    this.toggleTwo = this.toggleTwo.bind(this);
+    this.toggleThree = this.toggleThree.bind(this);
+    this.toggleFour = this.toggleFour.bind(this);
+    this.toggleFive = this.toggleFive.bind(this);
   }
 
   update(field) {
@@ -19,6 +29,57 @@ class ReviewForm extends React.Component {
         [field]: e.currentTarget.value
       });
     };
+  }
+
+  toggleOne () {
+    this.setState(
+      { oneStar: true,
+        twoStar: false,
+        threeStar: false,
+        fourStar: false,
+        fiveStar: false
+      }
+    );
+  }
+  toggleTwo () {
+    this.setState(
+      { oneStar: true,
+        twoStar: true,
+        threeStar: false,
+        fourStar: false,
+        fiveStar: false
+      }
+    );
+  }
+  toggleThree () {
+    this.setState(
+      { oneStar: true,
+        twoStar: true,
+        threeStar: true,
+        fourStar: false,
+        fiveStar: false
+      }
+    );
+  }
+  toggleFour () {
+    this.setState(
+      { oneStar: true,
+        twoStar: true,
+        threeStar: true,
+        fourStar: true,
+        fiveStar: false
+      }
+    );
+  }
+  toggleFive () {
+    this.setState(
+      { oneStar: true,
+        twoStar: true,
+        threeStar: true,
+        fourStar: true,
+        fiveStar: true
+      }
+    );
   }
 
   handleEdit(e) {
@@ -51,34 +112,33 @@ class ReviewForm extends React.Component {
         <div id="review-rating-radio">
           <p id="rating-indicator">Rating: </p>
 
+
+
+          <label className="label-one" htmlFor="rating-radio-1">
+            <div onClick={this.toggleOne} className={this.state.oneStar ? 'rated' : 'rating-star-box'}><i className="fa fa-star" aria-hidden="true"></i></div>
+          </label>
+
+          <label className="label-two"  htmlFor="rating-radio-2">
+            <div onClick={this.toggleTwo} className={this.state.twoStar ? 'rated' : 'rating-star-box'}><i className="fa fa-star" aria-hidden="true"></i></div>
+          </label>
+
+          <label  className="label-three" htmlFor="rating-radio-3">
+            <div onClick={this.toggleThree} className={this.state.threeStar ? 'rated' : 'rating-star-box'}><i className="fa fa-star" aria-hidden="true"></i></div>
+          </label>
+
+          <label className="label-four" htmlFor="rating-radio-4">
+            <div onClick={this.toggleFour} className={this.state.fourStar ? 'rated' : 'rating-star-box'}><i className="fa fa-star" aria-hidden="true"></i></div>
+          </label>
+
+          <label className="label-five" htmlFor="rating-radio-5">
+            <div onClick={this.toggleFive} className={this.state.fiveStar ? 'rated' : 'rating-star-box'}><i className="fa fa-star" aria-hidden="true"></i></div>
+          </label>
+
           <input id="rating-radio-1" type="radio" name="rating" value="1" onChange={this.update('rating')} />
-
-          <label htmlFor="rating-radio-1">
-            <div className={["rating-star-box","one","two", "three", "four", "five"].join(" ")}><i className="fa fa-star" aria-hidden="true"></i></div>
-          </label>
-
           <input id="rating-radio-2" type="radio" name="rating" value="2" onChange={this.update('rating')} />
-
-          <label  htmlFor="rating-radio-2">
-            <div className={["rating-star-box","two", "three", "four", "five"].join(" ")}><i className="fa fa-star" aria-hidden="true"></i></div>
-          </label>
-
           <input id="rating-radio-3" type="radio" name="rating" value="3" onChange={this.update('rating')} />
-          <label  htmlFor="rating-radio-3">
-            <div className={["rating-star-box","three", "four", "five"].join(" ")}><i className="fa fa-star" aria-hidden="true"></i></div>
-          </label>
-
           <input id="rating-radio-4" type="radio" name="rating" value="4" onChange={this.update('rating')} />
-
-          <label  htmlFor="rating-radio-4">
-            <div className={["rating-star-box","four", "five"].join(" ")}><i className="fa fa-star" aria-hidden="true"></i></div>
-          </label>
-
           <input id="rating-radio-5" type="radio" name="rating" value="5" onChange={this.update('rating')} />
-
-          <label  htmlFor="rating-radio-5">
-            <div className={["rating-star-box","five"].join(" ")}><i className="fa fa-star" aria-hidden="true"></i></div>
-          </label>
 
         </div>
         <input id="submit-review" type="submit" value="Submit Review!"/>
